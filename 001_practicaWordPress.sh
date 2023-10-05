fa6b101372878b9353f343af96ca78d2f7294d3e
alby <delcaidero@gmail.com>
2023-10-05 01:54:56 +0100

    build: first script

---
+++ 001_practicaWordPress.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "Installing multipass"
+sudo apt update
+sudo apt install -y multipass
+
+echo "available images"
+multipass find
+
+
+echo "https://multipass.run/docs/create-an-instance"
+
+echo "launch wordpress instance"
+multipass launch jammy --name wordpress --cpus 1 --disk 10G --memory 2G --network en0 --network name=bridge0,mode=manual
+
+echo "Wordpress instance info"
+multipass info wordpress
+
+echo "launch mysql instance"
+multipass launch jammy --name mysql --cpus 1 --disk 10G --memory 2G --network en0 --network name=bridge0,mode=manual
+
+echo "mysql instance info"
+multipass info mysql
+
+
+echo "Wordpress ip route"
+multipass exec wordpress -- ip route
